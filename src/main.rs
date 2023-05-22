44b91fb86865dab9e4fda77fa4eff0b4b07b6024
daniel daum <daniel_daum@outlook.com>
2023-05-22 18:38:10 -0500

    first rust project, init

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    // println!("Please input your guess.");
+
+    let secret_number = 10;
+
+    let mut guess = String::new(); //mutable
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
